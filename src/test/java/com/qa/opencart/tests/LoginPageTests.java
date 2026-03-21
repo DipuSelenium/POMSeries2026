fe07d2cea82cde48c2f75f232bd59219bc7262e6
DipuSelenium <dipu.kakati.2017@gmail.com>
2026-03-22 03:54:10 +0530

    Added Jenkinsfile

--- src/test/java/com/qa/opencart/tests/LoginPageTests.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTests.java
@@ -11,4 +11,24 @@ public class LoginPageTests extends BaseTest {
 		String titleString=loginPage.getLoginPageTitle();
 		Assert.assertEquals(titleString, "Account Login","Incorrect Page Title");
 	}
+//	@Test
+//	public void loginPageTitleTest1() {
+//		String titleString=loginPage.getLoginPageTitle();
+//		Assert.assertEquals(titleString, "Account Login","Incorrect Page Title");
+//	}
+//	@Test
+//	public void loginPageTitleTest2() {
+//		String titleString=loginPage.getLoginPageTitle();
+//		Assert.assertEquals(titleString, "Account Login","Incorrect Page Title");
+//	}
+//	@Test
+//	public void loginPageTitleTest3() {
+//		String titleString=loginPage.getLoginPageTitle();
+//		Assert.assertEquals(titleString, "Account Login","Incorrect Page Title");
+//	}
+//	@Test
+//	public void loginPageTitleTest4() {
+//		String titleString=loginPage.getLoginPageTitle();
+//		Assert.assertEquals(titleString, "Account Login","Incorrect Page Title");
+//	}
 }
